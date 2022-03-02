9234ed3616f0d6ac98c90e54b266429783e4eb6a
mehmetzahidpolatdemir@gmail.com.tr <mehmetzahidpolatdemir@gmail.com.tr>
2022-03-03 01:25:04 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var data = _context.Products.ToList();
+
+            return data;
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+
+
+            return _context.Products.Find(id); 
+        }
+    }
+}
