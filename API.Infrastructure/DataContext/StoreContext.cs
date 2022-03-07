3c29ef6ad9ee35a0445120b5d8eb61123052b59d
mehmetzahidpolatdemir@gmail.com.tr <mehmetzahidpolatdemir@gmail.com.tr>
2022-03-07 13:59:39 +0300

    Kurumsal N-Tier - Çok Katmanlı Mimariye Göre proje düzenlendi

---
+++ API.Infrastructure/DataContext/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using API.Core.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.DataContext
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
